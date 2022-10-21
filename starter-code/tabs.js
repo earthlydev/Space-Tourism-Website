97431df5696407cd43fda79130f820ab7293f038
earthlydev <jarizala.dev@gmail.com>
2022-10-20 19:49:24 -0700

    refactoring tab script

--- starter-code/tabs.js
+++ starter-code/tabs.js
@@ -1,12 +1,15 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
+tabList.addEventListener('keydown', changeTabFocus);
 
-tabList.addEventListener('keydown', changeTabFocus)
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel);
+});
+
 
 let tabFocus = 0;
-
-function changeTabFocus(e) {   
+function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
     
@@ -19,9 +22,7 @@ function changeTabFocus(e) {
         if (tabFocus >= tabs.length) {
             tabFocus = 0;
         }
-    }
-    
-    if (e.keyCode === keydownLeft) {
+    } else if (e.keyCode === keydownLeft) {
         tabFocus--;
         if (tabFocus < 0) {
             tabFocus = tabs.length - 1;
@@ -30,4 +31,37 @@ function changeTabFocus(e) {
     
     tabs[tabFocus].setAttribute("tabindex", 0);
     tabs[tabFocus].focus();
+}
+
+
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    const targetImage = targetTab.getAttribute("data-image");
+    
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+    
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
+        
+    targetTab.setAttribute("aria-selected", true);
+    
+    hideContent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, [`#${targetPanel}`]);
+    
+    hideContent(mainContainer, 'picture');
+    showContent(mainContainer, [`#${targetImage}`])
+}
+
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+    parent.querySelector(content).removeAttribute('hidden');
+
 }
